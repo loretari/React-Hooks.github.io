41eaf5d5ff53880d2ea1b7e5d5b212ba3f7e319d
loretari <loretari@gmail.com>
2023-07-04 09:32:33 +0300

    init: add prevInputState on useState( & State Updating

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -22,7 +22,19 @@ const submitHandler = event => {
                 type= 'text'
                 id= 'title'
                 value={inputState[0].title}
-                onChange={event => inputState[1] ({title: event.target.value})}/>
+                onChange={event => {
+                    const newTitle = event.target.value;
+                    inputState[1] (prevInputState => ({
+                            title: newTitle,
+                            amount: prevInputState.amount
+                        })
+                    )
+                }
+
+
+
+                }
+            />
         </div>
 <div className= 'form-control'>
     <label htmlFor= 'amount'>Amount</label>
@@ -30,7 +42,16 @@ const submitHandler = event => {
         type= 'number'
         id = 'amount'
         value={inputState[0].amount}
-        onChange={event => inputState[1] ({amount: event.target.value})}
+        onChange={event => {
+            const newAmount = event.target.value
+            inputState[1](prevInputState => ({
+                    amount: newAmount,
+                    title: prevInputState.title
+                })
+            )
+        }
+
+        }
     />
 </div>
         <div className= 'ingredient-form__actions'>
