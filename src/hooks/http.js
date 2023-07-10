e6f54799fb1cd8473f78fc684d15b2e63a849687
loretari <loretari@gmail.com>
2023-07-10 16:26:11 +0300

    init: using the Custom Hook

--- src/hooks/http.js
+++ src/hooks/http.js
@@ -1,5 +1,12 @@
 import { useReducer, useCallback } from 'react';
 
+const initialState = {
+    loading: false,
+    error: null,
+    data: null,
+    extra: null,
+    identifier: null
+};
 
 const httpReducer = (curHttpState, action) => {
     switch (action.type) {
@@ -19,9 +26,7 @@ const httpReducer = (curHttpState, action) => {
         case 'ERROR':
             return { loading: false, error: action.errorMessage };
         case 'CLEAR':
-            return {
-                ...curHttpState,
-                error: null };
+            return initialState;
         default:
             throw new Error('Should not be reached!');
     }
@@ -29,14 +34,9 @@ const httpReducer = (curHttpState, action) => {
 
 
 const useHttp = () => {
-    const [httpState, dispatchHttp] = useReducer(httpReducer,
-        {
-            loading: false,
-            error: null,
-            data: null,
-            extra: null,
-            identifier: null
-        });
+    const [httpState, dispatchHttp] = useReducer(httpReducer, initialState);
+
+    const clear = useCallback(() => dispatchHttp({ type: 'CLEAR' }), []);
 
     const sendRequest = useCallback((url, method, body, reqExtra, reqIdentifier) => {
         dispatchHttp({ type: 'SEND', identifier: reqIdentifier});
@@ -71,7 +71,8 @@ const useHttp = () => {
         error: httpState.error,
         sendRequest: sendRequest,
         reqExtra: httpState.extra,
-        reqIdentifier: httpState.identifier
+        reqIdentifier: httpState.identifier,
+        clear: clear
     };
 }
 
